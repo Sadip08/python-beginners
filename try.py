a8e0ef6003cc3d71a7e855edc1521a2db2407355
Sadip08 <tamangsadip08@gmail.com>
2024-02-29 18:37:56 +0545

    learnt Error handling

---
+++ try.py
@@ -0,0 +1,30 @@
+# try:
+#     f = open('testfile.txt', 'r')
+#     var = bad_var
+# except FileNotFoundError:
+#     print("Sorry. This file doesnot exist")
+# except Exception:
+#     print("something went wrong")
+#
+# try:
+#     f = open('testfile.txt', 'r')
+#     var = bad_var
+# except FileNotFoundError as e:
+#     print(e)
+# except Exception as e:
+#     print(e)
+#
+try:
+    f = open('test1.txt')
+    var = bad_var
+except FileNotFoundError as e:
+    print(e)
+except Exception as e:
+    print(e)
+else:
+    print(f.read())
+    f.close()
+finally:
+    print("Executing Finally")
+
+
